f21e2a5886474cc6b840c2a588272e50d7dfa79c
Sadik Al Jarif <78945097+jarif87@users.noreply.github.com>
2023-01-03 00:59:19 +0600

    Add files via upload

---
+++ nested_dictionary.py
@@ -0,0 +1,19 @@
+#%%
+dict0={1:"bangla",2:"english",3:{4:"math",5:"biology","name":"Tammanna"}}
+print(dict0)
+#%%
+type(dict0)
+# %%
+dict0[2]
+
+# %%
+dict0[3]
+# %%
+nested_dict = { 'dictA': {'key_1': 'value_1'},'dictB': {'key_2': 'value_2'}}
+print(nested_dict)
+
+nested_dict['dictA']
+# %%
+nested_dict["dictA"]["key_1"]
+
+# %%

a86c74aa14a903acb992d62262ab8276622f4108
Sadik Al Jarif <78945097+jarif87@users.noreply.github.com>
2023-01-04 02:51:29 +0600

    Add files via upload

---
+++ math_numbers.py
@@ -0,0 +1,46 @@
+#%%
+import math
+#%%
+a=8
+math.exp(a)
+# %%
+math.pow(a,2)
+# %%
+math.log(a)
+# %%
+math.log10(a)
+# %%
+math.sqrt(a)
+# %%
+math.factorial(a)
+# %%
+dir(math)
+# %%
+math.sin(a)
+# %%
+math.cos(a)
+# %%
+help(math)
+# %%
+import random
+# %%
+a=[1,2,3,4,56,6,7,8,9,10]
+# %%
+random.shuffle(a)
+# %%
+print(a)
+# %%
+print(random.randrange(1,100)) # print int values
+# %%
+print(random.uniform(2.20,4.55))
+# %%
+print(random.random())
+
+# %%
+random.seed(10)
+# %%
+print(random.random())
+
+# %%
+help(random)
+# %%

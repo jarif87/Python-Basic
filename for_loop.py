bf2a708f1c070c8e5fcf347da1f6c314a48c2235
Sadik Al Jarif <78945097+jarif87@users.noreply.github.com>
2023-01-04 02:35:17 +0600

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,38 @@
+#%%
+for i in "physics is my favourite subject":
+    if i in "subject":
+        break
+    else:
+        print(i)
+    
+    
+# %%
+for i in "physics is my favourite subject":
+    if i =="i":
+        break
+    else:
+        print(i)
+# %%
+for i in "physics is my favourite subject":
+    if i in "subject":
+        continue
+    else:
+        print(i)
+# %%
+for i in "physics is my favourite subject":
+    if i in "subject":
+        continue
+        print("String is :\n")  # never execute this line
+    
+    else:
+        print(i)
+
+# %%
+for i in "physics is my favourite subject":
+    if i in "subject":
+        pass
+        print("String is :\n")  # never execute this line
+    
+    else:
+        print(i)
+# %%

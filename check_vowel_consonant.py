0633115475a9103bfe70317dd1bcf10d1da01e39
Sadik Al Jarif <78945097+jarif87@users.noreply.github.com>
2023-01-04 02:05:49 +0600

    Add files via upload

---
+++ check_vowel_consonant.py
@@ -0,0 +1,8 @@
+
+
+check_char=input("Enter your character :\n")
+if(check_char=="A" or check_char=="a" or check_char=="E" or check_char=="e" or check_char=="I" or check_char=="i" or check_char=="O" or check_char=="o" or check_char=="U"or check_char=="u"):
+    print("Vowel")
+else:
+    print("Consonant")
+    

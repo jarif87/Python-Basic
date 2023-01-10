c844b09ed7865d4e832b96818d1126bfb70f9b63
Sadik Al Jarif <78945097+jarif87@users.noreply.github.com>
2023-01-11 02:26:01 +0600

    Add files via upload

---
+++ slicing_string.py
@@ -0,0 +1,63 @@
+#%%
+
+s="monty python"
+print(s[: 4])
+# %%
+print(s[6 :  7])
+# %%
+print(s[: ])
+# %%
+a="hi"
+b="anika"
+c= "hi" + "," +b
+print(a+b)
+# %%
+print(c)
+# %%
+word = "bananana"
+i = word.find("na")
+print(i)
+
+# %%
+# concating list
+a=[1,2,3,4,5]
+b=[2,3,4,5,6,7,8,9,0,11]
+print(a+b)
+# %%
+x=list()
+type(x)
+# %%
+dir(x)
+# %%
+stuff=list()
+stuff.append("bangla")
+
+stuff.append("book")
+stuff.append("pen")
+print(stuff)
+# %%
+words = 'His e-mail is q-lar@freecodecamp.org'
+pieces = words.split()
+parts = pieces[3].split('-')
+n = parts[1]
+
+# %%
+n
+# %%
+pieces
+# %%
+abc="with three words"
+x=abc.split(",")
+print(x)
+
+# %%
+abc="with three words"
+x=abc.split()
+print(x)
+# %%
+len(x)
+# %%
+c=" a              lot"
+v=c.split(", ")
+print(v)
+# %%
